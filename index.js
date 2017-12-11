5b33439cfb1c3cd92b06c9d4152c6578c2033e8a
Linda Foo <linda.foo.dev@gmail.com>
2017-12-11 09:50:02 -0800

    started Quiz App - working through StartApp

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-function startApp()
+//modify DOM - hit start then go to next view
+'use strict';
+
+const listQUESTIONS = [{
+  nameQuestion: "Question 1",
+  textQuestion: "textstring",
+  textOptions: "optionstring",
+  textAnswer: "answerstring",
+},
+{
+  nameQuestion: "Question 2",
+  textQuestion: "textstring",
+  textOptions: "optionstring",
+  textAnswer: "answerstring",
+},
+{
+  nameQuestion: "Question 3",
+  textQuestion: "textstring",
+  textOptions: "optionstring",
+  textAnswer: "answerstring",
+}
+];
+
+const STORE = {
+
+
+};
+//if app switch statement - start, question, answer, result
+//current state of start; then run start app
+//if in question state, put out quenstions, etc
+
+
+function startApp() {
+  $('.start').on('click', function(event){
+    console.log('testStart');
+    event.preventDefault();
+    $('h1').remove();
+    $('h2').remove();
+    //event.currentTarget.closest
+    $('.question').append(`<p> ${listQUESTIONS[0].textOptions} </p>`);
+    //console.log(listQUESTIONS[0].textOptions);
+    $('.view').append(`<button class="submit">Submit</button>`);
+    $('.start').remove();
+  });
+}
+
+startApp();
