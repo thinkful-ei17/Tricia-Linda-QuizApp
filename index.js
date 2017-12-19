b337e7ee4f8c6c5d09022eca9381daf8adb11897
Linda Foo <linda.foo.dev@gmail.com>
2017-12-18 17:53:27 -0800

    refactor code, pending buttons

--- index.js
+++ index.js
@@ -1,130 +1,136 @@
-//modify DOM - hit start then go to next view
 'use strict';
+/* global $ */
 
-const listQUESTIONS = [{
-  question: 'Question 1: What is the largest land animal in the world?',
-  a1: 'Elephant',
-  a2: 'Human',
-  a3: 'Hippo',
-  a4: 'Dinosaur',
-  answer: 'a1',
-  comment: function() {
-    return (this.al);
+const listQuestions = [
+  {
+    question: 'Question 1: What is the largest land animal in the world?',
+    option_one: 'Elephant', 
+    option_two: 'Human',
+    option_three: 'Hippo',
+    option_four: 'Dinosaur',
+    answer_option: 'option_one',
+    answer_text: 'Elephant',
+  },
+  {
+    question: 'Question 2: Groups of lions are known as what?',
+    option_one: 'Gangs',
+    option_two: 'Packs',
+    option_three: 'Pride',
+    option_four: 'Herd',
+    answer_option: 'option_two',
+    answer_text: 'Pride',
+  },
+  {
+    question: 'Question 3: What is the fastest land animal in the world?',
+    option_one: 'Leopard',
+    option_two: 'Cheetah',
+    option_three: 'Elephant',
+    option_four: 'Iganua',
+    answer: 'Cheetah',
+    answer_option: 'option_two',
+    answer_text: 'Cheetah',
+  },
+  {
+    question: 'Question 4: What can cats see that humans cannot?',
+    option_one: 'Ghosts',
+    option_two: 'Dust',
+    option_three: 'Sound',
+    option_four: 'Ultraviolet light',
+    answer_option: 'option_four',
+    answer_text: 'Ultraviolet light',
+
+  },
+  {
+    question: 'Question 5: How many hours can a puppy sleep a day?',
+    option_one: '5 to 20 hours',
+    option_two: '10 to 15 hours',
+    option_three: '18 to 20 hours',
+    option_four: '24 hours',
+    answer_option: 'option_three',
+    answer_text: '18 to 20 hours',
   }
-},
-{
-  question: 'Question 2: Groups of lions are known as what?',
-  a1: 'Gangs',
-  a2: 'Packs',
-  a3: 'Pride',
-  a4: 'Herd',
-  answer: 'a3',
-  comment: function() {
-    return (this.a3);
-  }
-},
-{
-  question: 'Question 3: What is the fastest land animal in the world?',
-  a1: 'Leopard',
-  a2: 'Cheetah',
-  a3: 'Elephant',
-  a4: 'Iganua',
-  answer: 'a2',
-  comment: function() {
-    return (this.a2);
-  }
-},
-{
-  question: 'Question 4: What can cats see that humans cannot?',
-  a1: 'Ghosts',
-  a2: 'Dust',
-  a3: 'Sound',
-  a4: 'Ultraviolet light',
-  answer: 'a4',
-  comment: function() {
-    return (this.a4);
-  }
-},
-{
-  question: 'Question 5: How many hours can a puppy sleep a day?',
-  a1: '5 to 20 hours',
-  a2: '10 to 15 hours',
-  a3: '18 to 20 hours',
-  a4: '24 hours',
-  answer: 'a3',
-  comment: function() {
-    return (this.a3);
-  }
-}
 ];
 
-const STORE = {
-  index: 0,
-  numberOfQuestions: listQUESTIONS.length,
-  questionNum: 1,
-  currentCorrectNum: 0,
+const STORE = { 
+  questionNum: 0,
+  numberOfQuestions: listQuestions.length,
+  questionCount: 1,
+  currentCorrectCount: 0, 
   view: 'start',
 };
 //if app switch statement - start, question, answer, result
 //current state of start; then run start app
 //if in question state, put out quenstions, etc
 
-
 function questionTemplate() {
   return `<form>
   <div>
-  <p class= "questionTitle">${listQUESTIONS[STORE.index].question}</p>
+  <p class= "questionTitle">${listQuestions[STORE.questionNum].question}</p>
     <input type="radio" id="Choice1"
-     name="answer" value="a1">
-    <label for="answerChoice1">${listQUESTIONS[STORE.index].a1}</label>
+     name="answer" value="option_one">
+    <label for="answerChoice1">${listQuestions[STORE.questionNum].option_one}</label>
 
     <input type="radio" id="Choice2"
-    name="answer" value="a2">
-   <label for="answerChoice1">${listQUESTIONS[STORE.index].a2}</label>
+    name="answer" value="option_two">
+   <label for="answerChoice1">${listQuestions[STORE.questionNum].option_two}</label>
 
     <input type="radio" id="Choice3"
-    name="answer" value="a3">
-    <label for="answerChoice1">${listQUESTIONS[STORE.index].a3}</label>
+    name="answer" value="option_three">
+    <label for="answerChoice1">${listQuestions[STORE.questionNum].option_three}</label>
 
     <input type="radio" id="Choice4"
-    name="answer" value="a4">
-  <label for="answerChoice1">${listQUESTIONS[STORE.index].a4}</label>
+    name="answer" value="option_four">
+  <label for="answerChoice1">${listQuestions[STORE.questionNum].option_four}</label>
   </div>
 </form>`;
 }
 
-function statusTemplate() {
-  console.log('Enter statusTemplate index = ', STORE.index);
-  return `<p> You have completed ${STORE.questionNum} out of ${STORE.numberOfQuestions}  total questions.</p><p> You have answered ${STORE.currentCorrectNum} correct, out of ${STORE.numberOfQuestions} total questions</p>`;
+function statusTemplate() {//pass as parameter and other templates!
+  return `<p> You have completed ${STORE.questionCount} out of ${STORE.numberOfQuestions}  total questions.</p><p> You have answered ${STORE.currentCorrectCount} correct, out of ${STORE.numberOfQuestions} total questions</p>`;
 }
 
-function resultTemplate(input) {
-  console.log('Enter resultTemplate index = ', STORE.index);
-  if (listQUESTIONS[STORE.index].answer === input) {
-    return '<p>Congratulations, your answer is correct</p>';
+function resultTemplate(feedback) {
+  if (listQuestions[STORE.questionNum].answer_option === feedback) {
+    return '<p> Congratulations, your answer is correct!</p>';
   } else {
-    return `<p>Your answer is incorrect, the correct answer is: ${listQUESTIONS[STORE.index].answer}</p>`;
+    
+    let correctAnswer = listQuestions[STORE.questionNum].answer_text; 
+    return `<p>Your answer is incorrect, the correct answer is: ${correctAnswer}</p>`;
   }
-
 }
 
-function renderPage() {
+function submitButtonTemplate() {
+  return '<button class="submit">Submit</button>';
+}
+
+function nextButtonTemplate() {
+  return '<button class="next">Next</button>';
+}
+
+//$('.buttons').append('<button class="submit">Submit</button>'); //move to template
+//$('.buttons').append('<button class="next">Next</button>'); //move to template
+
+
+function renderPage() { 
 
   if (STORE.view === 'start') {
     $('h1').show();
     $('h2').show();
     $('.start').show();
     $('.question').hide();
+    $('.result').hide();
     $('.status').hide();
     $('.submit').hide();
     $('.remove').hide();
     $('.next').hide();
     $('.form').hide();
+    displayQuestion();
   } else if (STORE.view === 'question') {
     $('h1').hide();
     $('h2').hide();
     $('.start').hide();
     $('.question').show();
+    $('.result').hide();
     $('.status').hide();
     $('.submit').show();
     $('.next').hide();
@@ -134,6 +140,7 @@ function renderPage() {
     $('h2').hide();
     $('.start').hide();
     $('.question').hide();
+    $('.result').show();
     $('.status').show();
     $('.submit').hide();
     $('.next').show();
@@ -144,6 +151,7 @@ function renderPage() {
     $('h2').show();
     $('.start').show();
     $('.question').hide();
+    $('.result').hide();
     $('.status').hide();
     $('.submit').hide();
     $('.remove').hide();
@@ -155,76 +163,32 @@ function renderPage() {
 function startApp() {
   handleSubmitClick();
   handleNextClick();
+  handleStartClick();
+}     
+
+function handleStartClick() {
   $('body').on('click', '.start', function(event) {
     event.preventDefault();
-    $('.buttons').append('<button class="submit">Submit</button>');
-    $('.buttons').append('<button class="next">Next</button>');
+   
     STORE.view = 'start';
     renderPage();
-    displayQuestion();
   }
   );
-}     
-  
-
-
-
-function displayQuestion() {
-  //display the question and 4 answers
-  //display the SUBMIT button
-  console.log('This index before we begin is:', STORE.index);
-  console.log('THe number of question or length is:', STORE.numberOfQuestions);
-
-  if (STORE.index < STORE.numberOfQuestions) {
-    console.log('Entered displayQuestion');
-    console.log('DisplayQuestion index = ', STORE.index);
-    let currentQuestion = questionTemplate();
-    $('.question').html(currentQuestion);
-    STORE.view = 'question';
-    renderPage();
-  }
-  else {
-    console.log('entered else statment');
-    STORE.index = 0;
-    STORE.questionNum = 1;
-    STORE.currentCorrectNum = 0;
-    STORE.view = 'restart';
-    renderPage();
-  }
-
 }
-
-
-//display current answer
-//display correctly answered
-function displayStatus() {
-  console.log('Entered displayStatus');
-  let status = statusTemplate();
-  $('.status').html(status);
-
-}
-
 
 function handleSubmitClick() {
-  console.log('Enter handleSumitClick');
-  $('body').on('click', '.submit', event => {
+  $('body').on('click', '.submit', function(event) {
     let clickResponse = $('input[name="answer"]:checked').val();
 
-    console.log('The index is = ', STORE.index);
-    console.log('What is the listQuestion answer:', listQUESTIONS[STORE.index].answer);
-    console.log('The clickResponse is = ', clickResponse);
-
-    if (clickResponse === listQUESTIONS[STORE.index].answer) {
-      STORE.currentCorrectNum += 1;
+    if (clickResponse === listQuestions[STORE.questionNum].answer_option) {
+      STORE.currentCorrectCount += 1;
     }
-    console.log(`HandleSubmit, Total correct answers =  ${STORE.currentCorrectNum}`);
 
     displayResults(clickResponse);
     displayStatus();
 
-    STORE.index += 1;
     STORE.questionNum += 1;
-    console.log('EnterSubmitClick, increment index =', STORE.index);
+    STORE.questionCount += 1;
     STORE.view = 'answer';
     renderPage();
 
@@ -232,23 +196,38 @@ function handleSubmitClick() {
 
 }
 
-function displayResults(input) {
-  console.log('Enter displayResults');
-  let results = resultTemplate(input);
-  $('.status').html(results);
-
-}
-
 function handleNextClick() {
-  //show the answer
-  //show answer
-
-  $('body').on('click', '.next', event => {
-    console.log('Enter handleNextClick index = ', STORE.index);
+  $('body').on('click', '.next', function(event) {
     displayQuestion();
   });
 
 }
 
+function displayQuestion() {
+  if (STORE.questionNum < STORE.numberOfQuestions) {
+    let currentQuestion = questionTemplate();
+    $('.question').html(currentQuestion);
+    STORE.view = 'question';
+    renderPage();
+  } 
+  else {
+    STORE.questionNum = 0; 
+    STORE.questionCount = 1;
+    STORE.currentCorrectCount = 0;
+    STORE.view = 'restart';
+    renderPage();
+  }
+}
+
+function displayStatus() {
+  let status = statusTemplate();
+  $('.status').html(status);
+
+}
+
+function displayResults(input) {
+  let results = resultTemplate(input);
+  $('.result').html(results);
+}
 
 $(startApp());
